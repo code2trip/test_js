8659fd8743275c8120710569d02580da2ea6bd2a
George Freedomnin <trilaev@yandex.ru>
2020-05-03 15:30:17 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+
+/*
+function goes (a, b, c){
+
+    return  console.log(a + b + c);
+
+};
+
+goes(100, 0.5, 15); */
+
+let persone = {
+
+    name:  "John",
+    age: 25,
+    isMerried: false
+
+};
+var arr = ['2', '2', '2', '2', '2', '2'];
+    for (let i = 0; i < arr.length; i++) {
+
+        console.log(arr[i] + " : " + i);
+
+    }
